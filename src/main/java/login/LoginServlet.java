8f497dbfdbbc1924df727a244360bc793da0a229
elissindricau <61435437+elissindricau@users.noreply.github.com>
2021-05-09 22:38:50 +0300

    Add files via upload

--- src/main/java/login/LoginServlet.java
+++ src/main/java/login/LoginServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import profile.ProfileBean;
+import profile.ProfileDao;
 import upper.info.UpperInfoBean;
 import upper.info.UpperInfoDao;
  
@@ -21,10 +23,12 @@ import upper.info.UpperInfoDao;
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private LoginDao loginDao;
+    private ProfileDao profileDao;
     public static Integer userID = null;
  
     public void init() {
         loginDao = new LoginDao();
+        profileDao = new ProfileDao();
     }
  
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -35,6 +39,7 @@ public class LoginServlet extends HttpServlet {
         LoginBean loginBean = new LoginBean();
         loginBean.setEmail(email);
         loginBean.setPassword(password);
+        
         //ArrayList<String> data = new ArrayList<String>();
  
         try {
@@ -44,12 +49,30 @@ public class LoginServlet extends HttpServlet {
                 request.setAttribute("id2",loginDao.validate(loginBean));
 	                System.out.println(loginDao.validate(loginBean));
 	                System.out.println(request.getAttribute("id2"));
+	                
 	            userID = loginDao.validate(loginBean);
+	            
+	            ProfileBean profile = new ProfileBean();
+	            try {
+	    			profile = ProfileDao.setProfile();
+	    		} catch (ClassNotFoundException e1) {
+	    			// TODO Auto-generated catch block
+	    			e1.printStackTrace();
+	    		}
+	            
                 UpperInfoDao.info((Integer)request.getAttribute("id2"));
                 request.setAttribute("nume",UpperInfoBean.getNume());
                 request.setAttribute("job",UpperInfoBean.getJob());
                 request.setAttribute("tip_user",UpperInfoBean.getTip_user());
                 request.setAttribute("companie",UpperInfoBean.getCompanie());
+                request.setAttribute("last_name", profile.getLastName());
+                	System.out.println("FIRST NAME: " + ProfileBean.getLastName());
+                request.setAttribute("first_name",profile.getFirstName());
+                request.setAttribute("job2",profile.getJob());
+                request.setAttribute("sex",profile.getSex());
+                request.setAttribute("birth_date",profile.getBirthDate());
+                request.setAttribute("email",profile.getEmail());
+                request.setAttribute("phone",profile.getPhone());
                 request.getRequestDispatcher("profile.jsp").forward(request, response);
                 //response.sendRedirect("profile.jsp");
             } else {
