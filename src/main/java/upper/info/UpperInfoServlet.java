ec2046ac51ff0080de4d9e3b4e9fec7dbf8d7b73
elissindricau <61435437+elissindricau@users.noreply.github.com>
2021-05-09 22:37:06 +0300

    Add files via upload

--- src/main/java/upper/info/UpperInfoServlet.java
+++ src/main/java/upper/info/UpperInfoServlet.java
@@ -1,18 +1,22 @@
 package upper.info;
-
+ 
 import java.io.IOException;
 import java.util.ArrayList;
-
+ 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+ 
 import login.LoginBean;
 import login.LoginDao;
+import login.LoginServlet;
 import registration.EmployeeBean;
-
+ 
+ 
+@WebServlet("/upperInfo")
 public class UpperInfoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     private UpperInfoDao upperinfodao;
@@ -21,16 +25,37 @@ public class UpperInfoServlet extends HttpServlet {
     	upperinfodao = new UpperInfoDao();
     }
  
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	try {
+    		String button = request.getParameter("button");
  
-    	request.setAttribute("nume",UpperInfoBean.getNume());
-    	System.out.println(request.getAttribute("nume"));
-    	request.setAttribute("job",UpperInfoBean.getJob());
-    	System.out.println(request.getAttribute("job"));
-    	request.setAttribute("tip_user",UpperInfoBean.getTip_user());
-    	System.out.println(request.getAttribute("tip_user"));
-    	System.out.println("hellooo");
-    	
+			UpperInfoDao.info(LoginServlet.userID);
+			//System.out.println("UPPERINFO");
+			if ("profil".equals(button)) {
+		request.setAttribute("nume",UpperInfoBean.getNume());
+        request.setAttribute("job",UpperInfoBean.getJob());
+        request.setAttribute("tip_user",UpperInfoBean.getTip_user());
+        request.setAttribute("companie",UpperInfoBean.getCompanie());
+        request.getRequestDispatcher("profile.jsp").forward(request, response);
+        }else if("taskuri".equals(button)) {
+        	request.setAttribute("nume",UpperInfoBean.getNume());
+            request.setAttribute("job",UpperInfoBean.getJob());
+            request.setAttribute("tip_user",UpperInfoBean.getTip_user());
+            request.setAttribute("companie",UpperInfoBean.getCompanie());
+            request.getRequestDispatcher("taskuri.jsp").forward(request, response);
+        }else if("employees_management".equals(button)) {
+        	request.setAttribute("nume",UpperInfoBean.getNume());
+            request.setAttribute("job",UpperInfoBean.getJob());
+            request.setAttribute("tip_user",UpperInfoBean.getTip_user());
+            request.setAttribute("companie",UpperInfoBean.getCompanie());
+            request.getRequestDispatcher("employees_management.jsp").forward(request, response);
+        }
+ 
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+ 
+ 
     }
 }
